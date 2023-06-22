9574cc526bce741fb5c70f83fd9f2b18df545f60
mikhail <mishanya-101@yandex.ru>
2023-06-22 17:41:15 +0400

    fixed: cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,14 +1,39 @@
-const cors = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+const allowedCors = [
+  'http://localhost:3000',
+  'http://api.mikhail.yandex.nomoredomains.rocks',
+  'https://api.mikhail.yandex.nomoredomains.rocks',
+  'http://mikhail.yandex.nomoredomains.rocks',
+  'https://mikhail.yandex.nomoredomains.rocks',
+];
+
+const cors = ((req, res, next) => {
+  const { origin } = req.headers;
   const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    return res.end();
+    res.end();
   }
-  return next();
-};
+
+  next();
+});
+
+// const cors = (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   const { method } = req;
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers'];
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     return res.end();
+//   }
+//   return next();
+// };
 
 module.exports = cors;
